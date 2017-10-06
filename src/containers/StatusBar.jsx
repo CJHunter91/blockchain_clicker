48d6807dbc17cb5f83620f9aec11d9bdf7981a71
Chris Hunter <orkem2@gmail.com>
2017-10-06 13:56:40 +0100

    adds status bar container

---
+++ src/containers/StatusBar.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class StatusBar extends Component{
+
+  render(){
+    return(
+        <section>
+          <ul>
+            <li>Coin: 100</li>
+          </ul>
+        </section>
+      )
+  }
+}
+
+export default StatusBar;
