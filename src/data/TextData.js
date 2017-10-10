a539c3be2bd724fa66321ce8dea818e68009afb6
Chris Hunter <orkem2@gmail.com>
2017-10-10 20:20:16 +0100

    adds more information data

--- src/data/TextData.js
+++ src/data/TextData.js
@@ -7,11 +7,31 @@ const textData = [
 }, 
 {
   id: 2,
-  text: "Mining a block, just like in a real blockchain, can potentially reward you with an amount of crypto currency."
+  text: "Mining a block, just like in a real blockchain, can potentially reward you with an amount of crypto currency. You can spend this Coin in the shop to increase your processing power."
 },
 {
   id: 3,
-  text: "Blah blah"
+  text: "Blockchains can be used in lots of ways this example uses Bitcoin. The blockchain monitors Bitcoin movements from user to user but can monitor transfer of other data."
+},
+{
+  id: 4,
+  text: "A block can be split into the block header and the block body.The block header is hashed which creates a unique hash for that block. Each time the nonce is incremented the hash also changes."
+},
+{
+  id: 5,
+  text: "The block is validated when the hash number is below an agreed upon difficulty number. Multiple users will be trying to successfully hash the next block and the first to do so receives the reward."
+},
+{
+  id: 6,
+  text: "The prevHash refers to the previous blocks hash which creates the chain. Each block will be linked back to the first gensis block"
+},
+{
+  id: 7,
+  text: "The more miners there are the harder the difficulty. It requires a lot of energy. This stops anyone from taking control of the blockchain, assuming they have less than 51% of the global processing power."
+},
+{
+  id: 8,
+  text: "All transactions on the Bitcoin blockchain are publically avaiable creating transparent transactions. Bitcoins can only be created by the mining rewards. This controls the global number of bitcoins"
 }
 
 ]
