26088e155d8817739d3178eaf40e98142757439f
Chris Hunter <orkem2@gmail.com>
2018-11-18 11:46:55 +0000

    fixes BlockTransactions conditional

--- src/components/BlockTransactions.jsx
+++ src/components/BlockTransactions.jsx
@@ -2,28 +2,25 @@ import React from 'react';
 
 const BlockTransactions = (props) => {
 
-  var transactions = () => {
-    return(
-      <p>No Transactions</p>
-      )
-  }
-  if(props.transactions.length >= 0){
-    transactions = props.transactions.map((trans, index) => {
-      return(
-        <li key={index}>{trans}</li>
-        )
-    })
+  const transactions = () => {
+    return (
+      <ul className="list">
+        {props.transactions.map((transaction, index) => {
+          return (
+            <li key={index}>{transaction}</li>
+          )
+        })
+        }
+      </ul>
+    )
   }
 
-
-  return(
+  return (
     <article id="block-transactions">
       <h4>Transactions</h4>
-      <ul className="list">
-        {transactions}
-      </ul>
+      {props.transactions.length > 0 ? transactions() : <p>No previous transactions</p>}
     </article>
-    )
+  )
 }
 
 export default BlockTransactions;
