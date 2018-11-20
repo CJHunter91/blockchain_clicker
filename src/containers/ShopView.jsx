52ebdf4020de1e78f4949e2dc8f457d07372d233
Chris Hunter <chrisjhunter91@gmail.com>
2018-11-20 11:21:35 +0000

    moves shop css to shopView file

--- src/containers/ShopView.jsx
+++ src/containers/ShopView.jsx
@@ -8,7 +8,7 @@ const ShopView = (props) => {
             <li key={index}>{resource.name} Cost: {resource.cost}
                 <button key={index} onClick={() => { props.userBuy(resource) }} className="buttons">
                     Buy
-    </button>
+                </button>
             </li>
         )
     })
@@ -16,52 +16,17 @@ const ShopView = (props) => {
     if (props.isOpen === false)
         return null
 
-    let modalStyle = {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        height: '80%',
-        width: '80%',
-        transform: 'translate(-50%, -50%)',
-        zIndex: '9999',
-        background: '#9ad3de',
-        borderRadius: '10px',
-        border: 'solid white 2px'
-    }
-
-    if (props.style) {
-        for (let key in props.style) {
-            modalStyle[key] = props.style[key]
-        }
-    }
-
-    let backdropStyle = {
-        position: 'absolute',
-        width: '100%',
-        height: '100%',
-        top: '0px',
-        left: '0px',
-        zIndex: '9998',
-        background: 'rgba(0, 0, 0, 0.3)'
-    }
-
-    if (props.backdropStyle) {
-        for (let key in props.backdropStyle) {
-            backdropStyle[key] = props.backdropStyle[key]
-        }
-    }
-
     return (
 
         <article >
-            <section id="shop-list" className="modal-content" style={modalStyle}>
+            <section id="shop-list" className="modal-content">
                 <span id="close" onClick={props.closeModal} className="close">&times;</span>
                 <h4>Bits and Bytes Computer Shop</h4>
                 <ul>
                     {resources}
                 </ul>
             </section>
-            <div style={backdropStyle} onClick={e => props.closeModal(e)} />
+            <div className="backdrop" onClick={e => props.closeModal(e)} />
         </article>
     )
 }
